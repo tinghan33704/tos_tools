7557cf5b87f64a008bdaa767bf38a7488b285da4
tinghan33704 <tinghan33704@gmail.com>
2024-02-26 20:43:24 +0800

    add Lin Daiyu easter egg

--- tos_skill_filter/js/index.js
+++ tos_skill_filter/js/index.js
@@ -1220,7 +1220,11 @@ function renderMonsterImage(monster, tooltip_content, monsterObj, eggLink = fals
 	const starburstBackground = monster.id === 10495 ? 'star-burst-popover' : 'popover'
 	const congratClickListener = monster.id === 10495 ? `'congratCounter()'` : 'null'
 	
-	const src_path = digimonChouShinka ? `../tos_tool_data/img/monster/${monster_obj.id}_sp2.png` : digimonShinka ? `../tos_tool_data/img/monster/${monster_obj.id}_sp1.png` : anyaSmile ? `../tos_tool_data/img/monster/${monster_obj.id}_sp.png` : sashaEat ? `../tos_tool_data/img/monster/empty_${attr_zh_to_en[monster_obj.attribute]}.png` : cilantroAngry ? `../tos_tool_data/img/monster/${monster_obj.id}_sp.png` : `../tos_tool_data/img/monster/${hasImageChange ? hasImageChange[0] : monster_obj.id}.png`
+	const currentTime = new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Taipei' }))
+	const passedMinutesFromToday = currentTime.getHours() * 60 + currentTime.getMinutes()
+	const isNowNight = monster.id === 10622 && (passedMinutesFromToday < 6 * 60 || passedMinutesFromToday >= 18 * 60)
+	
+	const src_path = digimonChouShinka ? `../tos_tool_data/img/monster/${monster_obj.id}_sp2.png` : digimonShinka ? `../tos_tool_data/img/monster/${monster_obj.id}_sp1.png` : anyaSmile ? `../tos_tool_data/img/monster/${monster_obj.id}_sp.png` : sashaEat ? `../tos_tool_data/img/monster/empty_${attr_zh_to_en[monster_obj.attribute]}.png` : cilantroAngry ? `../tos_tool_data/img/monster/${monster_obj.id}_sp.png` : isNowNight ? `../tos_tool_data/img/monster/${monster_obj.id}_sp.png` : `../tos_tool_data/img/monster/${hasImageChange ? hasImageChange[0] : monster_obj.id}.png`
 	const error_path = `../tos_tool_data/img/monster/noname${monster_attr?.length ? `_${attr_zh_to_en[monster_attr]}` : ''}.png`
 	const focus_path = hasImageChange ? `../tos_tool_data/img/monster/${hasImageChange[1]}.png` : hasSpecialImage ? `../tos_tool_data/img/monster/${monster_obj.id}_sp.png` : src_path
 	const blur_path = hasImageChange ? `../tos_tool_data/img/monster/${hasImageChange[0]}.png` : src_path
